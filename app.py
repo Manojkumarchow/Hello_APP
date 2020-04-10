2dbba38926650d93cd9834a89ea3a9bb7458fae1
Manoj Kumar <manojchow72@gmail.com>
2020-04-10 11:30:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,39 +13,39 @@ app.config['UPLOAD_FOLDER'] = PEOPLE_FOLDER
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return "Hello World"
 
 
-@app.route("/diabetes")
-def main():
-    return render_template("home.html")
+# @app.route("/diabetes")
+# def main():
+#     return render_template("home.html")
 
-@app.route("/predict", methods = ['GET', 'POST'])
-def predict():
-    if request.method == "POST":
-        try:
-            Pregnancies = int(request.form['Pregnancies'])
-            Glucose = int(request.form['Glucose'])
-            BloodPressure = int(request.form['BloodPressure'])
-            SkinThickness = int(request.form['SkinThickness'])
-            Insulin = int(request.form['Glucose'])
-            BMI = float(request.form['Glucose'])
-            DiabetesPedigreeFunction = float(request.form['DiabetesPedigreeFunction'])
-            Age = int(request.form['Age'])
-            pred_args = [Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]
-            pred_args_arr = np.array(pred_args)
-            pred_args_arr = pred_args_arr.reshape(1, -1)
-            svm = open('model.pkl', 'rb')
-            ml_model = joblib.load(svm)
-            model_prediction = ml_model.predict(pred_args_arr)
-            if model_prediction == 1:
-                model_prediction = "You have Diabetes, please consult a Doctor."
-            elif model_prediction == 0:
-                model_prediction = "You don't have Diabetes."
-        except ValueError:
-            return "Please check whether the values are entered correctly"
+# @app.route("/predict", methods = ['GET', 'POST'])
+# def predict():
+#     if request.method == "POST":
+#         try:
+#             Pregnancies = int(request.form['Pregnancies'])
+#             Glucose = int(request.form['Glucose'])
+#             BloodPressure = int(request.form['BloodPressure'])
+#             SkinThickness = int(request.form['SkinThickness'])
+#             Insulin = int(request.form['Glucose'])
+#             BMI = float(request.form['Glucose'])
+#             DiabetesPedigreeFunction = float(request.form['DiabetesPedigreeFunction'])
+#             Age = int(request.form['Age'])
+#             pred_args = [Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]
+#             pred_args_arr = np.array(pred_args)
+#             pred_args_arr = pred_args_arr.reshape(1, -1)
+#             svm = open('model.pkl', 'rb')
+#             ml_model = joblib.load(svm)
+#             model_prediction = ml_model.predict(pred_args_arr)
+#             if model_prediction == 1:
+#                 model_prediction = "You have Diabetes, please consult a Doctor."
+#             elif model_prediction == 0:
+#                 model_prediction = "You don't have Diabetes."
+#         except ValueError:
+#             return "Please check whether the values are entered correctly"
 
-    return render_template("predict.html", prediction = model_prediction)
+#     return render_template("predict.html", prediction = model_prediction)
     # return "Heyy!"
 
 
